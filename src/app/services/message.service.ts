5e81adfbee3b88ea167e47fd3856b8b47531af5e
bryan <bryan.barbou@hotmail.fr>
2021-03-17 09:20:36 +0100

    ADD Message
    Possible to see and clear the messages

--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -4,6 +4,17 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class MessageService {
+  messages:{message:string}[] =[];
 
-  constructor() { }
+  constructor() {
+    this.messages.push({message:"ENFIN"})
+    this.messages.push({message:"UN"})
+    this.messages.push({message:"message"})
+  }
+  log(message:string){
+    this.messages.push({message:message});
+  }
+  clear(){
+    this.messages=[];
+  }
 }
